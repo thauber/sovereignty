1bebeec9dd9910acc81a13aac76710cb6f30f2d2
Tony Hauber <thauber@gmail.com>
2009-01-19 00:53:48 -0800

    added models and views, simple

--- nations/views.py
+++ nations/views.py
@@ -1,1 +1,23 @@
 # Create your views here.
+
+
+def nation(request, nation_slug):
+    """docstring for nation"""
+    pass
+
+def issue(request, issue_id):
+    """docstring for issue"""
+    pass
+
+def allies(request, nation_slug):
+    pass
+
+def enemies(request, nation_slug):
+    pass
+
+def stats(request, nation_slug):
+    pass
+
+
+
+
